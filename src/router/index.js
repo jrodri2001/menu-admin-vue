929356eaa7c10e16c2e6ba6bf7625c0d40c2fb63
Julio Rodriguez <juliojrg@gmail.com>
2018-05-23 16:26:55 -0400

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const routerOptions = [
+  { path: '/', component: 'Landing' },
+  { path: '/menu', component: 'MenuManager' },
+  { path: '/signin', component: 'SignIn' },
+  { path: '/home', component: 'Home' }
+]
+
+const routes = routerOptions.map(route => {
+  return {
+    ...route,
+    component: () => import(`@/components/${route.component}.vue`)
+  }
+})
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes
+})
